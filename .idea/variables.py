eda972dad20aee990d28bfb9d4aead55b9d5f3cf
irinabelova001 <105107203+irinabelova001@users.noreply.github.com>
2022-05-24 14:50:48 +0300

    Add files via upload

---
+++ .idea/variables.py
@@ -0,0 +1,18 @@
+# Объект сотрудник
+
+# fio тип данных строка
+fio="Иванов Иван Иванович"
+
+# god_r тип данных целый
+god_r=1989
+
+# stag_rab тип данных вещественный
+stag_rab=3.8
+
+# sertf тип данных логический
+sertf=True
+
+print("ФИО: ",fio,type(fio),sep="   ")
+print("Год рождения:",god_r,type(god_r),sep= "    ")
+print("Стаж работы: ",stag_rab, type(stag_rab),sep="    ")
+print("Сертификат:  ",sertf, type(sertf),sep="    ")
